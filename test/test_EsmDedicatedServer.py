e068a23151d0ff430aa7ea35262fd5914afa130b
Vollinger <148711063+Vollinger0@users.noreply.github.com>
2023-10-16 02:15:22 +0200

    extended tests with mode retrieving methods

--- test/test_EsmDedicatedServer.py
+++ test/test_EsmDedicatedServer.py
@@ -12,7 +12,6 @@ class TestEsmDedicatedServer(unittest.TestCase):
     log = logging.getLogger(__name__)
 
     def test_createLogFileName(self):
-        # esmDS = EsmDedicatedServer(installDir="..", dedicatedYaml="esm-dedicated.yaml")
         esmConfig = EsmConfig.fromConfigFile('test/esm-test-config.yaml')
         esmDS = EsmDedicatedServer(esmConfig)
         logFileName = esmDS.createLogFileName()
@@ -20,3 +19,20 @@ class TestEsmDedicatedServer(unittest.TestCase):
         logFileNameLast4 = logFileName[-4:]
         self.assertEqual("../Logs/4243/Dedicated_", logFileNameFirst23)
         self.assertEqual(".log", logFileNameLast4)
+
+    def test_getGfxModeByString(self):
+        esmConfig = EsmConfig.fromConfigFile('test/esm-test-config.yaml')
+        esmDS = EsmDedicatedServer(esmConfig)
+        mode = esmDS.getGfxModeByString(True)
+        self.assertEqual(mode, EsmDedicatedServer.GFXMODE_ON)
+        mode = esmDS.getGfxModeByString(False)
+        self.assertEqual(mode, EsmDedicatedServer.GFXMODE_OFF)
+
+    def test_getStartModeByString(self):
+        esmConfig = EsmConfig.fromConfigFile('test/esm-test-config.yaml')
+        esmDS = EsmDedicatedServer(esmConfig)
+        mode = esmDS.getStartModeByString('direct')
+        self.assertEqual(mode, EsmDedicatedServer.STARTMODE_DIRECT)
+        mode = esmDS.getStartModeByString('launcher')
+        self.assertEqual(mode, EsmDedicatedServer.STARTMODE_LAUNCHER)
+
