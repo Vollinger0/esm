4bf00ac5f176f9b0364d8062c729e2c19ac80962
Vollinger <148711063+Vollinger0@users.noreply.github.com>
2023-10-16 02:15:27 +0200

    added proper startandwait command, fixed some timings

--- src/esm/EsmLogger.py
+++ src/esm/EsmLogger.py
@@ -3,15 +3,15 @@ import logging
 class EsmLogger:
 
     @staticmethod 
-    def setUpLogging(logFile):
+    def setUpLogging(logFile, fileLogLevel=logging.DEBUG, streamLogLevel=logging.DEBUG):
         # set a nice logging line and logging level, including proper logging to a logfile
         fileLoggingHandler = logging.FileHandler(logFile)
         # debug level for the logfile
-        fileLoggingHandler.setLevel(logging.DEBUG)
+        fileLoggingHandler.setLevel(fileLogLevel)
         streamHandler = logging.StreamHandler()
         # info level for the console
         # streamHandler.setLevel(logging.INFO)
-        streamHandler.setLevel(logging.DEBUG)
+        streamHandler.setLevel(streamLogLevel)
         logging.basicConfig(
             format="[%(asctime)s] %(levelname)s %(message)s", 
             datefmt='%Y-%m-%d %H:%M:%S',
