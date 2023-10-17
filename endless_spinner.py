b0d30adadff5dec88cf7d02f81a91da5d76779b0
Vollinger <148711063+Vollinger0@users.noreply.github.com>
2023-10-17 21:51:00 +0200

    added endless spinner for testing

---
+++ endless_spinner.py
@@ -0,0 +1,11 @@
+from halo import Halo
+
+print("Starting sleep phase, press CTRL+C to end.")
+with Halo(text="Running", spinner="dots") as spinner:
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        spinner.succeed("Stopped")
+
+print("The end.")
