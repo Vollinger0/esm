45f68c4ff581aad2356795f54e10ec3447c0b262
Vollinger <volleymail+vollinger@gmail.com>
2023-11-05 05:00:31 +0100

    implemented proper ingame chat messaging and tests

--- test/test_EsmCommunicationService.py
+++ test/test_EsmCommunicationService.py
@@ -1,13 +1,10 @@
 
 import logging
-import subprocess
-import time
 import unittest
 
-from esm.EsmCommunicationService import EsmCommunicationService, Priority
+from esm.EsmCommunicationService import EsmCommunicationService
 from esm.EsmConfigService import EsmConfigService
 from esm.ServiceRegistry import ServiceRegistry
-
 
 log = logging.getLogger(__name__)
 
@@ -32,21 +29,3 @@ class test_EsmCommunicationService(unittest.TestCase):
         log.debug(f"end: {end}")
         self.assertIsNotNone(start)
         self.assertIsNotNone(end)
-
-
-    @unittest.skip("only for manual execution, since you won't see anything if you aren't looking ingame.")
-    def test_serverChatAndAnnouncements(self):
-        esmConfig = EsmConfigService(configFilePath='esm-base-config.yaml')
-        ServiceRegistry.register(esmConfig)
-        cs = ServiceRegistry.get(EsmCommunicationService)
-        # no way to assert this, start the game and look at the chat.
-        cs.serverChat(message=f"hello from esm, dear watcher of the server chat!", quietMode=False)
-        time.sleep(3)
-        cs.announce(message="alert from an esm test", priority=Priority.ALERT, quietMode=False)
-        time.sleep(3)
-        cs.announce(message="warning from an esm test", priority=Priority.WARNING, quietMode=False)
-        time.sleep(3)
-        cs.announce(message="info from an esm test", priority=Priority.INFO, quietMode=False)
-        time.sleep(3)
-        cs.announce(message="just some text from an esm test", priority=Priority.OTHER, quietMode=False)
-
---
+++ test/test_EsmEpmRemoteClientService.py
@@ -0,0 +1,51 @@
+
+import logging
+import time
+import unittest
+
+from esm.EsmCommunicationService import EsmCommunicationService
+from esm.EsmConfigService import EsmConfigService
+from esm.EsmEpmRemoteClientService import Channel, EsmEpmRemoteClientService, Priority, SenderType
+from esm.ServiceRegistry import ServiceRegistry
+
+
+log = logging.getLogger(__name__)
+
+class test_EsmEpmRemoteClientService(unittest.TestCase):
+
+    @unittest.skip("only for manual execution, since you won't see anything if you aren't looking ingame.")
+    def test_serverChatAndAnnouncements(self):
+        esmConfig = EsmConfigService(configFilePath='esm-base-config.yaml')
+        ServiceRegistry.register(esmConfig)
+        epmrc = ServiceRegistry.get(EsmEpmRemoteClientService)
+        # no way to assert this, start the game and look at the chat.
+        epmrc.sendServerChat(message=f"hello from esm, dear watcher of the server chat!", quietMode=False)
+        time.sleep(2)
+        epmrc.sendAnnouncement(message="alert from an esm test", priority=Priority.ALERT, quietMode=False)
+        time.sleep(2)
+        epmrc.sendAnnouncement(message="warning from an esm test", priority=Priority.WARNING, quietMode=False)
+        time.sleep(2)
+        epmrc.sendAnnouncement(message="info from an esm test", priority=Priority.INFO, quietMode=False)
+        time.sleep(2)
+        epmrc.sendAnnouncement(message="just some text from an esm test", priority=Priority.OTHER, quietMode=False)
+        time.sleep(2)
+        epmrc.sendMessage(senderName="The first tester", message="woohooo! test succeeded!")
+        time.sleep(2)
+        epmrc.sendMessage(senderName="The[0affe0]colored[deadbe]Tester", message="[78ab56]so[ab5678]many[5678ab]colors")
+        time.sleep(2)
+        self.debugSend(epmrc)
+        
+
+    def debugSend(self, epmrc: EsmEpmRemoteClientService):
+        epmrc.sendMessage(senderName="SendertypeShowcase", message="*all sender types and no sender name*")
+        for senderType in SenderType:
+            epmrc.sendMessage(message=f"i am senderType {senderType}", senderType=senderType)
+            time.sleep(.3)
+        epmrc.sendMessage(senderName="SendertypeShowcase", message="*sending all sender types with set name*")
+        for senderType in SenderType:
+            epmrc.sendMessage(senderName="FooBar", message=f"i am senderType {senderType}", senderType=senderType)
+            time.sleep(.3)
+        epmrc.sendMessage(senderName="ChanneltypeShowcase", message="*sending at all channel types*")
+        for channel in Channel:
+            epmrc.sendMessage(senderName="Tester2", message=f"i am channel type {channel}", channel=channel)
+            time.sleep(.3)
