91a95d2d7f03c43cd71c10955ff40a3760589feb
Vollinger0 <148711063+Vollinger0@users.noreply.github.com>
2024-02-24 16:03:58 +0100

    shared data zip file is now properly created

--- src/esm/ConfigModels.py
+++ src/esm/ConfigModels.py
@@ -127,6 +127,7 @@ class DownloadToolConfig(BaseModel):
     cacheFolderName: str = Field("DediGame_127.0.0.1_123456789", description="name of the folder included in the zip file, which will look something like 'DediGame_127.0.0.1_12346789', depending on gamename, server ip and seed")
     zipName: str = Field("shareddata_copy_to_saves_cache", description="The filename of the zip file that will be provided as download")
     wwwroot: str = Field("wwwroot", description="folder to use as wwwroot, where the download will be served from")
+    tempFolder: str = Field("temp", description="temporary folder for creating the downloadable zip")
 
 class MainConfig(AppBaseModel, AppConfigMixin):
     general: ConfigGeneral = Field(ConfigGeneral())
--- src/esm/EsmSharedDataServer.py
+++ src/esm/EsmSharedDataServer.py
@@ -19,6 +19,31 @@ class EsmSharedDataServer:
     def start(self):
         scenarioName = self.config.dedicatedConfig.GameConfig.CustomScenario
         pathToScenarioFolder = Path(f"{self.config.paths.install}/Content/Scenarios/{scenarioName}").resolve()
+
+        log.info(f"Creating new shared data zip file from the current configured scenario at '{pathToScenarioFolder}'")
+        resultZipFilePath = self.createSharedDataZipFile(pathToScenarioFolder)
+        wwwrootZipFilePath = self.moveSharedDataZipFileToWwwroot(resultZipFilePath)
+        log.info(f"Created SharedData zip file as '{wwwrootZipFilePath}' using the cache folder name '{self.config.downloadtool.cacheFolderName}'")
+
+        # TODO: start webserver on configured port and serve the zip, maybe also with a dynamic index.html that explains how to handle the shared data
+        # TODO: log that the server is running and when someone downloads the zip
+        log.info("would have started the download server now, but its not implemented yet")
+
+    def moveSharedDataZipFileToWwwroot(self, resultZipFilePath: Path) -> Path:
+        wwwroot = Path(self.config.downloadtool.wwwroot).resolve()
+        if not wwwroot.exists():
+            FsTools.createDir(wwwroot)
+
+        wwwrootZipFilePath = wwwroot.joinpath(f"{self.config.downloadtool.zipName}.zip").resolve()
+        if wwwrootZipFilePath.exists():
+            log.debug(f"Deleting old zip file at '{wwwrootZipFilePath}'")
+            FsTools.deleteFile(wwwrootZipFilePath)
+        log.debug(f"Moving zip file '{resultZipFilePath}' to '{wwwroot}'")
+        shutil.move(resultZipFilePath, wwwroot)
+        log.debug(f"result of zip creation: '{wwwrootZipFilePath}'")
+        return wwwrootZipFilePath
+
+    def createSharedDataZipFile(self, pathToScenarioFolder: Path) -> Path:
         # just using something smaller for debugging
         pathToSharedDataFolder = pathToScenarioFolder.joinpath("SharedData/Content/Extras")
         #pathToSharedDataFolder = pathToScenarioFolder.joinpath("SharedData")
@@ -27,18 +52,19 @@ class EsmSharedDataServer:
             log.warning(f"Path to the shared data in the games scenario folder '{pathToSharedDataFolder}' does not exist. Please check the configuration.")
             return
         
-        wwwroot = Path(self.config.downloadtool.wwwroot).resolve()
-        if not wwwroot.exists():
-            FsTools.createDir(wwwroot)
+        tempFolder = Path(self.config.downloadtool.tempFolder).resolve()
+        if tempFolder.exists():
+            log.debug(f"deleting old temporary folder '{tempFolder}'")
+            FsTools.deleteDir(tempFolder, True)
+        FsTools.createDir(tempFolder)
+        cacheFolder = tempFolder.joinpath(self.config.downloadtool.cacheFolderName)
+        FsTools.createDir(cacheFolder)
         
-        # create zip from the shared data folder with the configured folder name
-        log.debug(f"Creating zip from shared data folder at path '{pathToSharedDataFolder}' with name '{self.config.downloadtool.zipName}.zip'")
-        result = shutil.make_archive(self.config.downloadtool.zipName, 'zip', base_dir="", root_dir=pathToSharedDataFolder)
+        log.debug(f"Copying files from '{pathToSharedDataFolder}' to cachefolder '{cacheFolder}'")
+        FsTools.copyDir(source=pathToSharedDataFolder, destination=cacheFolder)
+        
+        # create zip from the cacheFolder
+        log.debug(f"Creating zip from cachefolder '{cacheFolder}' with name '{self.config.downloadtool.zipName}.zip'")
+        result = shutil.make_archive(self.config.downloadtool.zipName, 'zip', tempFolder)
         resultZipFilePath = Path(result)
-        shutil.move(resultZipFilePath, wwwroot)
-        wwwrootZipFilePath = wwwroot.joinpath(f"{self.config.downloadtool.zipName}.zip").resolve()
-        log.debug(f"result of zip creation: {wwwrootZipFilePath}")
-
-        # TODO: start webserver on configured port and serve the zip, maybe also with a dynamic index.html that explains how to handle the shared data
-        # TODO: log that the server is running and when someone downloads the zip
-        log.info("would have started the download server now, but its not implemented yet")
+        return resultZipFilePath
